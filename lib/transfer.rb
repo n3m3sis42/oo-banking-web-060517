c303ecc24880c883e9e0a05a9299d6b1ed60fb1f
n3m3sis42 <n3m3sis42@gmail.com>
2017-06-13 08:14:55 -0400

    done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,46 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+      @sender = sender
+      @receiver = receiver
+      @amount = amount
+      @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    case
+    when !self.valid?
+      reject_transaction("Invalid sending or receiving bank account. Transfer rejected.")
+    when @sender.balance < @amount
+      reject_transaction("Transaction rejected. Please check your account balance.")
+    when @status == "complete"
+      "Transaction already complete. Please initiate a new transfer to move more money."
+    else
+      process_transaction
+    end
+  end
+
+  def reject_transaction(error_msg)
+    @status = "rejected"
+    error_msg
+  end
+
+  def process_transaction
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  end
+
+  def reverse_transfer
+    return if status != "complete"
+    @sender.balance += @amount
+    @receiver.balance -= @amount
+    @status = "reversed"
+  end
+
 end
